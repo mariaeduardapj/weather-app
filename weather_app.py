3fc679dd23472349e48d8e46cdaaca3ba5326d98
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-07-01 22:11:38 -0300

    style: Window redesign

--- weather_app.py
+++ weather_app.py
@@ -2,8 +2,42 @@ import tkinter as tk
 import requests
 import os
 from dotenv import load_dotenv
+from PIL import Image, ImageTk
 
 load_dotenv()
+
+gif_frames=[]
+gif_delay=0
+gif_label=None
+
+def play_gif(label, frames, delay, current_frame_index=0):
+    if not frames:
+        return
+    frame = frames[current_frame_index]
+    label.config(image=frame)
+    current_frame_index=(current_frame_index+1) % len(frames)
+    label.after(delay, play_gif, label, frames, delay, current_frame_index)
+
+def load_gif(file_path):
+    global gif_frames, gif_delay
+    try:
+        image=Image.open(file_path)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return False
+    except Exception as e:
+        print(f"Error loading GIF: {e}")
+        return False
+    temp_frames=[]
+    try:
+        while True:
+            temp_frames.append(ImageTk.PhotoImage(image.copy()))
+            image.seek(len(temp_frames))
+    except EOFError:
+        pass
+    gif_frames=temp_frames
+    gif_delay=image.info.get('duration',100)
+    return True
 
 def get_weather():
     city = city_entry.get()
@@ -21,14 +55,34 @@ def get_weather():
         result_label.config(text="City not found.")
 
 root = tk.Tk()
-root.title("Current weather")
-root.geometry("300x200")
+root.title("Weather App 🌦️")
+root.geometry("400x400")
+root.configure(bg="#ffe6f0")
 
-city_entry = tk.Entry(root, font=("Arial", 14))
-city_entry.pack(pady=10)
+gif_file="C:\\Users\\duda2\\Estudos\\Projetos\\weather-app\\f.gif"
+if load_gif(gif_file):
+    gif_label=tk.Label(root,bg="#ffe6f0")
+    gif_label.pack(pady=10)
+    play_gif(gif_label, gif_frames, gif_delay)
+else:
+    error_gif_label=tk.Label(root, text="Could not load GIF",bg="#ffe6f0")
+    error_gif_label.pack(pady=10)
 
-btn = tk.Button(root, text="Search weather", command=get_weather)
+city_entry = tk.Entry(root, font=("Arial", 14), bg="#ffffff")
+city_entry.pack(pady=15)
+
+btn = tk.Button(
+    root,
+    text=" Check Weather ", 
+    font=("Arial",12,"bold"),
+    bg="#ff99cc",
+    fg="#ffffff",
+    activebackground="#ff66b2",
+    activeforeground="#ffffff",
+    bd=0,
+    command=get_weather)
 btn.pack()
+
 
 result_label = tk.Label(root, font="Arial, 12")
 result_label.pack(pady=20)
