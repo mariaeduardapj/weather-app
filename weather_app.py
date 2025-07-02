782db4b95431f53e855a72e8606dbe6893cbc7b2
Maria Eduarda Pereira de Jesus <mariaeduardapereiraje@gmail.com>
2025-07-01 21:00:24 -0300

    feat: Hide the API key

--- weather_app.py
+++ weather_app.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_weather():
     city = city_entry.get()
-    api_key = "198fe609b196b760c4b992bb486c1d0a"
+    api_key = os.getenv("API_KEY")
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=pt_br"
 
     response = requests.get(url)         
